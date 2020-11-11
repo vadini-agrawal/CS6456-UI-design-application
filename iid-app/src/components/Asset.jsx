50ef033cea9b6eefbd2a11c60bbe0156f19d7225
vagrawal48 <vadini@gatech.edu>
2020-11-11 13:17:58 -0500

    Added audio

--- iid-app/src/components/Asset.jsx
+++ iid-app/src/components/Asset.jsx
@@ -1,51 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
+
+import '../style/HomePage.css';
 
 function Asset(props)
 {   
-    let modalOpen = false;
+    // const [show, setShow] = useState(false);
 
-    const modalClose = (e) => {
-        modalOpen = false;
-    }
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => {
+    //     setShow(true);
+    // }
 
-    const openModal= () => {
-        modalOpen = true;
-    }
+    let assetWidth = props.data.width;
+    let assetHeight = props.data.height;
 
-    const handleDoubleClick= () => {
-        console.log("Somethign is happening");
-        openModal();
-        // props.assetSizeHandler(e.currentTarget);
-    }
+    // const handleDoubleClick= () => {
+    //     console.log("Somethign is happening");
+    //     handleShow();
+    //     // props.assetSizeHandler(e.currentTarget);
+    // }
+
+    // let default_img = <img onDoubleClick={handleDoubleClick} alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>;
+
+    // const submitSize = () => {
+    //     assetWidth = document.getElementById("changeAssetWidth").value;
+    //     assetHeight = document.getElementById("changeAssetHeight").value;
+    //     default_img = <img onDoubleClick={handleDoubleClick} alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>;
+    //     handleClose();
+    // }
 
     return (
-        <div>
-        <Modal show={modalOpen}>
-        <div className="form-group">
-        <label>Enter Height (in inches) </label>
-        <input 
-            type="number"
-            // value={this.state.changeAssetHeight}
-            // name="changeAssetHeight"
-            // onChange={e => this.handleChange(e)}
-            // className="form-control"
-        />
-        <label>Enter Width (in inches) </label>
-        <input 
-            type="number"
-            // value={this.state.changeAssetWidth}
-            // name="changeAssetWidth"
-            // onChange={e => this.handleChange(e)}
-            // className="form-control"
-        />
-        <button  className="modal_button" type="button">
-        Save
-        </button>
+        <>
+       {/* <Modal show={show} onHide={handleClose}>
+         <div className="form-group">
+            <label>Enter Height (in inches) (Current value: {assetHeight}) </label>
+            <input 
+                id="changeAssetHeight"
+                type="number"
+                // value={assetHeight}
+                name="changeAssetHeight"
+                className="form-control"
+            />
+            <label>Enter Width (in inches) (Current value: {assetWidth} </label>
+            <input 
+                id="changeAssetWidth"
+                type="number"
+                // value={assetWidth}
+                name="changeAssetWidth"
+                className="form-control"
+            />
+            <button  className="modal_button" onClick={submitSize} type="button">Save</button>
+            <button  className="modal_button" onClick={handleClose} type="button">Close</button>
         </div>
-         </Modal>       
-        <img onDoubleClick={handleDoubleClick} alt={""} src={props.data.image_url} width={props.data.width*12} height={props.data.height*12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>
-        </div>
+        </Modal> */}
+        {/* { default_img } */}
+        <img alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>
+        </>
     );
 }
 
