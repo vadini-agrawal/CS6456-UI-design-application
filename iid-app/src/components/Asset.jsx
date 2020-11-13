958d76624d931ac8a4f737d5a19bdef7f90be4e1
Vadini Agrawal <31671738+vadini-agrawal@users.noreply.github.com>
2020-11-13 11:15:10 -0500

    Final changes (#32)
    Cleaned up all of the code

--- iid-app/src/components/Asset.jsx
+++ iid-app/src/components/Asset.jsx
@@ -1,62 +1,12 @@
 import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 
-import '../style/HomePage.css';
-
-function Asset(props)
-{   
-    // const [show, setShow] = useState(false);
-
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => {
-    //     setShow(true);
-    // }
-
+function Asset(props) {
     let assetWidth = props.data.width;
     let assetHeight = props.data.height;
 
-    // const handleDoubleClick= () => {
-    //     console.log("Somethign is happening");
-    //     handleShow();
-    //     // props.assetSizeHandler(e.currentTarget);
-    // }
-
-    // let default_img = <img onDoubleClick={handleDoubleClick} alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>;
-
-    // const submitSize = () => {
-    //     assetWidth = document.getElementById("changeAssetWidth").value;
-    //     assetHeight = document.getElementById("changeAssetHeight").value;
-    //     default_img = <img onDoubleClick={handleDoubleClick} alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>;
-    //     handleClose();
-    // }
-
     return (
-        <>
-       {/* <Modal show={show} onHide={handleClose}>
-         <div className="form-group">
-            <label>Enter Height (in inches) (Current value: {assetHeight}) </label>
-            <input 
-                id="changeAssetHeight"
-                type="number"
-                // value={assetHeight}
-                name="changeAssetHeight"
-                className="form-control"
-            />
-            <label>Enter Width (in inches) (Current value: {assetWidth} </label>
-            <input 
-                id="changeAssetWidth"
-                type="number"
-                // value={assetWidth}
-                name="changeAssetWidth"
-                className="form-control"
-            />
-            <button  className="modal_button" onClick={submitSize} type="button">Save</button>
-            <button  className="modal_button" onClick={handleClose} type="button">Close</button>
-        </div>
-        </Modal> */}
-        {/* { default_img } */}
         <img alt={""} src={props.data.image_url} width={assetWidth * 12} height={assetHeight * 12} draggable="true" data-iswallasset={props.data.isWallAsset.toString()}/>
-        </>
     );
 }
 
--- iid-app/src/components/Canvas.jsx
+++ iid-app/src/components/Canvas.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import { Stage, Layer, Image, Rect} from 'react-konva';
-// import {Button} from "react-bootstrap";
 import useImage from 'use-image';
 import AssetMenu from './AssetMenu';
 import TrashCanImage from './TrashCanImage';
@@ -14,11 +13,6 @@ import '../style/AssetMenu.css';
 
 const URLImage = ({id, image, height, width, onDragEnd, onDragStart, originalX, originalY, onDblClick, iswallasset }) => {
   const [img] = useImage(image.src);
-  // let image_node = React.useRef();
-  // const y_coord = image.y;
-  // const [spring] = useSpring({y: 10, from: {y: y_coord}})
-  // const image_component = 
-  // const AnimatedImage = animated(image_component)
 
   return (
     <Image
@@ -62,12 +56,9 @@ const Canvas = (props) => {
   const stageRef = React.useRef();
   const isWallAsset = React.useRef();
   const [images, setImages] = React.useState([]);
-  const [nodes, setNodes] = React.useState([]);
-
   
   useEffect(() => {
     if (!images) {
-      console.log('run something here');
     }
   }, [images]);
 
@@ -94,19 +85,6 @@ const Canvas = (props) => {
     } 
     return lowestY - height + height / 4;
   };
-
-  // const moveHigherImages = (images) => {
-  //   // let i = 0;
-  //   // let new_images = images;
-  //   // for (i = 0; i < new_images.length; i++) {
-  //   //   let lowestY = findLowestY(new_images, new_images[i].x, new_images[i].height, new_images[i].width, new_images[i].id);
-  //   //   if (new_images[i].y < lowestY) {
-  //   //     let img = new_images[i];
-  //   //     img.y = lowestY;
-  //   //     new_images[i] = img;
-  //   //   }
-  //   return images;
-  // }
 
   const findImageId = (id) => {
     let i = 0; 
@@ -155,7 +133,6 @@ const Canvas = (props) => {
             const drop = document.getElementById("drop_audio");
             drop.play();
           }
-          console.log(images);
         }
     }
   }
@@ -164,8 +141,6 @@ const Canvas = (props) => {
   }
 
   const handleDoubleClk = (e) => {
-    console.log("doubleClick");
-    console.log(e);
     props.assetSizeHandler(e.currentTarget);
   }
 
